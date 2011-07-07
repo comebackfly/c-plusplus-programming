58792aaa090a56a820f844acf4ef3b6a0c65c707
samsalabim@live.de <samsalabim@live.de@5f9f56c3-fb77-04ef-e3c5-e71eb3e36737>
2011-07-07 17:10:57 +0000

    first step-not all ready

---
+++ WatchingEyes/WatchingEyes/Eye.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WatchingEyes
+{
+    class Eye
+    {
+
+        private int positionX;
+        private int positionY;
+        private int size;
+        private int status;
+        private Pupil pupil;
+        private Graphics g;
+
+        public Eye(int positionX, int positionY, int size, Pupil pupil)
+        {
+            this.positionX = positionX;
+            this.positionY = positionY;
+            this.size=size;
+            this.pupil = pupil;
+        }
+
+        public void paintEye(Graphics g)
+        {
+            this.g = g;
+            // Create a new pen that we shall use for drawing the line
+            Pen myPen = new Pen(Color.Black);
+            //MessageBox.Show(""+this.positionX+" "+this.positionY+" "+this.size);
+
+            g.DrawEllipse(myPen, this.positionX, this.positionY, this.size, this.size/2);
+
+            pupil.paintPupil(g);
+        }
+
+        public void close()
+        {
+            this.pupil.setUnvisible();
+
+        }
+
+        public int getPositionX()
+        {
+            return this.positionX;
+        }
+
+        public int getPositionY()
+        {
+            return this.positionY;
+        }
+
+        public int getWidth()
+        {
+
+            return this.size;
+        }
+
+        public Pupil getPupil()
+        {
+            return this.pupil;
+        }
+    }
+}
---
+++ WatchingEyes/WatchingEyes/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WatchingEyes
+{
+    public partial class Form1 : Form
+    {
+        private Eye leftEye;
+        private Eye rightEye;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //create eyes with pupils
+            int eyeSize = 70;
+            int pupilSize=10;
+            Pupil leftPupil = new Pupil();
+            leftEye = new Eye(((this.Width / 2) - eyeSize), ((this.Height / 2) - (eyeSize / 2)), eyeSize, leftPupil);
+            leftPupil.setPupil(leftEye.getPositionX()+pupilSize/2, leftEye.getPositionY()+pupilSize/2, pupilSize);
+
+            Pupil rightPupil = new Pupil();
+            rightEye = new Eye(((this.Width / 2)), ((this.Height / 2) - (eyeSize / 2)), eyeSize, rightPupil);
+            rightPupil.setPupil(rightEye.getPositionX()+pupilSize/2, rightEye.getPositionY()+pupilSize/2, pupilSize);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label.Text = e.X.ToString() + ", " + e.Y.ToString();
+
+            if(e.X<=leftEye.getPositionX()+leftEye.getWidth() && e.X>=leftEye.getPositionX()&&e.Y<=leftEye.getPositionY()+leftEye.getWidth()&&e.Y>=leftEye.getPositionY()){
+                leftEye.getPupil().updatePupil(e.X, e.Y);
+            }
+            else if (e.X <= rightEye.getPositionX() + rightEye.getWidth() && e.X >= rightEye.getPositionX() && e.Y <= rightEye.getPositionY() + rightEye.getWidth() && e.Y >= rightEye.getPositionY())
+            {
+                rightEye.getPupil().updatePupil(e.X, e.Y);
+            }
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            label.Text = "mouse leaving the window";
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Get Graphics Object
+            Graphics g = e.Graphics;
+            leftEye.paintEye(g);
+            rightEye.paintEye(g);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            switch(e.Button){
+
+                case MouseButtons.Left: leftEye.close();
+                    break;
+
+                case MouseButtons.Right: rightEye.close();
+                    break;
+            }
+
+        }
+    }
+}
