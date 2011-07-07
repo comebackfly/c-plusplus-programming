c17127d3721a7bdeed04fce7f2ce891863896f7e
samsalabim@live.de <samsalabim@live.de@5f9f56c3-fb77-04ef-e3c5-e71eb3e36737>
2011-07-07 17:10:57 +0000

    first step-not all ready

---
+++ trunk/WatchingEyes/WatchingEyes/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WatchingEyes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(149, 43);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 13);
+            this.label.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 285);
+            this.Controls.Add(this.label);
+            this.Name = "Form1";
+            this.Text = "Watching Eyes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ trunk/WatchingEyes/WatchingEyes/Pupil.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WatchingEyes
+{
+    class Pupil
+    {
+        private int positionX;
+        private int positionY;
+        private int size;
+        private Graphics g;
+        private SolidBrush pupilBrush;
+
+        public Pupil()
+        {
+            pupilBrush = new SolidBrush(Color.LawnGreen);
+        }
+
+        public void setPupil(int positionX, int positionY, int size)
+        {
+            this.positionX = positionX;
+            this.positionY = positionY;
+            this.size=size;
+        }
+
+        public void paintPupil(Graphics g){
+            this.g = g;
+            // Create a new pen that we shall use for drawing the line
+            
+            //MessageBox.Show(""+this.positionX+" "+this.positionY+" "+this.size);
+            if (this.positionX != 0)
+            {
+                g.FillEllipse(pupilBrush, this.positionX, this.positionY, this.size, this.size);
+            }
+        }
+
+        public void updatePupil(int positionX, int positionY)
+        {
+            if (positionX == 0 || positionY == 0)
+            {
+                g.FillEllipse(pupilBrush, this.positionX, this.positionY, this.size, this.size);
+            }
+            else
+            {
+                g.FillEllipse(pupilBrush, positionX, positionY, this.size, this.size);
+            }
+          
+        }
+
+        public void setUnvisible()
+        {
+            SolidBrush invisible = new SolidBrush(Color.Teal);
+            g.FillEllipse(invisible, this.positionX, this.positionY, this.size, this.size);
+        }
+    }
+}
